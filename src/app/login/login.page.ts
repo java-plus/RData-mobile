016c589a3fe93dbd4a663df13225ca5092bc0c2d
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-10-15 14:36:18 +0200

    page login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../services/login.service';
-import { Form, NgForm } from '@angular/forms';
+import {  NgForm } from '@angular/forms';
 
+/**
+   * représente la page login de l'application
+   * 
+   * 
+   */
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: [],
 })
 export class LoginPage implements OnInit {
 
+  // La variable "messageError" est nulle au chargement de la page.
+  // Si l'utilisateur renseigne un mauvais identifiant, mot de passe ou tout ce qui pourrait empecher la connexion
+  // Elle s'initialise à 'Connexion impossible avec cet identifiant et ce mot de passe' et est affichée en rouge dans le html
   messageError:string;
-  messageTentative:string;
 
+  /**
+   * Le constructeur initialise les singletons router (pour la redirection vers d'autres urls)
+   * et loginService (pour utiliser les méthodes présentes dans ce service)
+   */
   constructor(private router:Router,private loginService:LoginService) { }
 
   ngOnInit() {
   }
   
+  // La methode authentification permet de faire un POST au back contenant les informations du formulaire
+  // Elle fait appel au service login.service via la méthode authentification(identifiant, motDePasse).
+  // Si la requete renvoi une erreur, alors la variable this.messageError prend la valeur d'un message d'erreur qui sera affichée dans le front
+  // Si la requete ne renvoie pas d'erreur, l'utilisateur est redirigée vers une url vue qui correspond à l'url "/secure/meteo"
   authentification(form:NgForm) {
     
     this.loginService.authentification(form.value.name, form.value.password).subscribe(() => {
@@ -27,13 +42,6 @@ export class LoginPage implements OnInit {
       
     });
   }
-  test(form:NgForm){
-    console.log(form.value.name)
-    console.log(form.value.email)
-    console.log(form.value.password)
-    console.log(form.value.confirm)
-
-    console.log(form.value)
-  }
+  
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Form } from '@angular/forms';
 import { environment } from 'src/environments/environment.prod';
 import {HttpHeaders, HttpClient} from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import Utilisateur from '../model/Utilisateur';
 import { Router } from '@angular/router';
-import {catchError, flatMap, tap} from 'rxjs/operators';
+import { flatMap, tap} from 'rxjs/operators';
 
+// Constante de racine url, en local = http://localhost:8080, en prod: https://airdata.cleverapps.io
 const URL_BACKEND = environment.backendUrl;
+
+// Constante d'options pour les requetes http
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
@@ -15,19 +17,44 @@ const httpOptions = {
   withCredentials: true
 };
 
+/**
+   * représente les services liés au login de l'application
+   * 
+   * 
+   */
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  /**
+   * Ce constructeur initialise les singletons 
+   * _http (pour permettre les requetes sur le Back)
+   * router (pour permettre la redirection vers d'autres urls)
+   */
   constructor(private http:HttpClient, private router: Router) { }
 
+  /**
+   *Ce subject transmet aux autres composants l'utilisateur connecté 
+   *  
+   */
   private _subConnecte: BehaviorSubject<Utilisateur> = new BehaviorSubject(undefined);
 
+  
+  /**
+   *methode get permettant d'avoir accès au subject _subConnecte
+   */
   get subConnecte(): Observable<Utilisateur> {
     return this._subConnecte.asObservable();
   }
 
+/**
+   * Cette methode permet de s'authentifier auprès du back via une requete POST contenant l'identifiant et le mot de passe 
+   * renseignés dans le formulaire de la page /login.
+   * Si la requete d'authentification ne renvoi pas d'erreur alors une nouvelle requete Get est générée pour obtenir l'object Utilisateur Connecté
+   * Cette utilisateur est transmis aux autres composants via _subConnecte qui est un BehaviorSubject
+   * 
+   */
   authentification(nomUtilisateur, motDePasse): Observable<Utilisateur>{
 
     return this.http
@@ -42,7 +69,6 @@ export class LoginService {
         return this.http.get<Utilisateur>(`${URL_BACKEND}/auth/user`, {withCredentials: true});
       }), tap((utilisateur) => {
           this._subConnecte.next(utilisateur);
-          this.router.navigate(['accueil']);
         })
       );
 
