814989b00a99d543e5d5f9dc99a434a8974097ec
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-10-15 16:25:59 +0200

    favoris

--- src/app/favoris/favoris.page.ts
+++ src/app/favoris/favoris.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FavorisService} from '../services/favoris.service';
+import Favori from '../model/Favori';
 
 @Component({
     selector: 'app-favoris',
@@ -7,35 +9,30 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FavorisPage implements OnInit {
 
-    public items: any = [];
+    public listeObjetFavori: { favori: Favori, expanded: boolean }[] = [];
 
-    constructor() {
-        this.items = [
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false},
-            {expanded: false}
-        ];
+    constructor(private favorisService: FavorisService) {
     }
 
     ngOnInit() {
+        this.favorisService.recupererFavorisUserConnecte().subscribe((listeFavori) => {
+            this.listeObjetFavori = listeFavori.map((fav) => {
+                return {favori: fav, expanded: false};
+            });
+        });
     }
 
-    expandItem(item): void {
-        if (item.expanded) {
-            item.expanded = false;
+    expandItem(objetFavori): void {
+        if (objetFavori.expanded) {
+            objetFavori.expanded = false;
         } else {
-            this.items.map(listItem => {
-                if (item === listItem) {
-                    listItem.expanded = !listItem.expanded;
+            this.listeObjetFavori.map(objFav => {
+                if (objetFavori === objFav) {
+                    objFav.expanded = !objFav.expanded;
                 } else {
-                    listItem.expanded = false;
+                    objFav.expanded = false;
                 }
+                return objFav;
             });
         }
     }
