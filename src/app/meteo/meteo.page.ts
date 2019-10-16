e017e01b4dc8fe7948d7adcc729c1a2cde91aa00
Eloi Turpin <eloi.turpin@gmail.com>
2019-10-16 09:31:44 +0200

    meteo en cours

--- src/app/meteo/meteo.page.ts
+++ src/app/meteo/meteo.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-meteo',
   templateUrl: './meteo.page.html',
-  styleUrls: [],
+  styleUrls: ['./meteo.page.scss'],
 })
+
+
 export class MeteoPage implements OnInit {
 
   constructor() { }
 
+  lat: number;
+  long: number;
+
+
+  // iconEtatMeteo: string;
+  iconEtatMeteo = '11d';
+  urlIconMeteo = 'https://openweathermap.org/img/wn/' + this.iconEtatMeteo + '@2x.png';
+
+  getCurrentLocation() {
+    Plugins.Geolocation.getCurrentPosition().then(position => {
+      this.lat = position.coords.latitude;
+      this.long = position.coords.longitude;
+      console.log('lat : ', this.lat, ' / long : ', this.long);
+    });
+  }
+
+
   ngOnInit() {
+    this.getCurrentLocation();
   }
 
 }
