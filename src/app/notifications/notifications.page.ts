4c64a763924e23c907733bc18a5c5dd3d1f19ce4
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-10-16 17:39:29 +0200

    notifications

--- src/app/notifications/notifications.page.ts
+++ src/app/notifications/notifications.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import Favori from '../model/Favori';
+import {GeolocalisationService} from '../services/geocalisation.service';
+
+/**
+ * represente un objet de la liste de notification
+ */
+interface ObjetNotification {
+  notification: Notification;
+  expanded: boolean;
+}
+
 
 @Component({
   selector: 'app-notifications',
@@ -8,37 +19,43 @@ import { Component, OnInit } from '@angular/core';
 export class NotificationsPage implements OnInit {
 
   /**
-   * liste de favoris
+   * liste de Notification
    */
-  listeNotifications: Notifi[] = [];
+  listeNotifications: ObjetNotification[] = [];
   /**
    * message d’erreur
    */
   messageError: string;
 
-  constructor() { }
+  constructor(private geolocService: GeolocalisationService) { }
 
   ngOnInit() {
+    this.geolocService.recupererGeoLocEtCommune().subscribe((infosCommune) => {
+      const stringLocalisation = infosCommune.features[0].properties.context.split(',').map(chaine => chaine.trim());
+      const codeCommune = infosCommune.features[0].properties.citycode;
+      const codeDepartement = stringLocalisation[1];
+      const codeRegion = stringLocalisation[2];
+
+    });
+
   }
 
 
   /**
-   * fonction déclenché lorsque l’on clique sur un favori pour l’ouvrir ou le fermer
-   * et déclenche la récuperation des mesures météo et pollution
-   * @param objetFavori favori selectionné par l’utilisateur
+   * fonction déclenché lorsque l’on clique sur une notification pour l’ouvrir ou le fermer
+   * @param objetNotification notification selectionné par l’utilisateur
    */
-  expandItem(objetFavori): void {
-    this.recupererMesureFavori(objetFavori.favori.commune.codeCommune);
-    if (objetFavori.expanded) {
-      objetFavori.expanded = false;
+  expandItem(objetNotification): void {
+    if (objetNotification.expanded) {
+      objetNotification.expanded = false;
     } else {
-      this.listeObjetFavori.map(objFav => {
-        if (objetFavori === objFav) {
-          objFav.expanded = !objFav.expanded;
+      this.listeNotifications.map(objNot => {
+        if (objetNotification === objNot) {
+          objNot.expanded = !objNot.expanded;
         } else {
-          objFav.expanded = false;
+          objNot.expanded = false;
         }
-        return objFav;
+        return objNot;
       });
     }
   }
