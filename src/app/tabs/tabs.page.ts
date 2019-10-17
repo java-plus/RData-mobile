555342835093611c9a68630f5cd7814945a3751e
Eloi Turpin <eloi.turpin@gmail.com>
2019-10-17 14:59:50 +0200

    ajout alerte confirmation de déconnexion

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -9,7 +10,29 @@ import { Router } from '@angular/router';
 })
 export class TabsPage {
 
-  constructor(private loginService: LoginService, private router: Router) { }
+  constructor(private loginService: LoginService, private router: Router, public alertController: AlertController) { }
+
+
+  async alerteConfirmationDeconnexion() {
+    const alert = await this.alertController.create({
+      header: 'Déconnexion',
+      message: 'Vous allez être déconnecté',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary'
+        }, {
+          text: 'Confimer',
+          handler: () => {
+            this.deconnecter();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   /**
    * deconnexion de l’utilisateur  et redirection vers la page de login
